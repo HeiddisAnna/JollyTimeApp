fbeb06e7837776a0be0b85daca3ed527eaa1fb12
Marin McGinley <ga48pum@mytum.de>
2019-02-25 21:54:42 +0000

    sign in útlit komið

--- app/src/main/java/is/hi/hbv601g/jollytime/MainActivity.java
+++ app/src/main/java/is/hi/hbv601g/jollytime/MainActivity.java
@@ -2,12 +2,39 @@ package is.hi.hbv601g.jollytime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private String email;
+    private String password;
+
+    private Button signInButton;
+    private EditText emailInput;
+    private EditText passwordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signInButton = (Button) findViewById(R.id.signin_button);
+        emailInput = (EditText) findViewById(R.id.email_input);
+        passwordInput = (EditText) findViewById(R.id.password_input);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = emailInput.getText().toString();
+                password = passwordInput.getText().toString();
+            }
+        });
+
+
     }
+
+
 }
