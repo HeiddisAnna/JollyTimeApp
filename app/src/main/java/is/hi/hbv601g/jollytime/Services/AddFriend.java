d3ca8a33ff6422f67a40912837228c31b9cc0b1f
bds8 <bds8@hi.is>
2019-03-11 20:48:42 +0000

    friends

---
+++ app/src/main/java/is/hi/hbv601g/jollytime/Services/AddFriend.java
@@ -0,0 +1,13 @@
+package is.hi.hbv601g.jollytime.Services;
+
+public class AddFriend {
+    private String name;
+    private String password;
+    private String email;
+    private ArrayList<Event> events;
+    private ArrayList<Event> dates;
+    private ArrayList<User> friends;
+    private ArrayList<Notification> notifications;
+}
+
+
